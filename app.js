5aaf738544d39a919d8da66ec7345b6315ddf837
hdzgabriel <hdzgabriel@gmail.com>
2015-05-08 21:14:59 -0500

    Web Interface
    Se agrega la interfaz web —aun en desarrollo—

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var logger = require('./util/logger');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -27,7 +27,8 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/app', express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api/consultoras', consultoras);
